6b045ab01ffb2e943372cf75e0559f592f38e92d
Edwin Chavarría (Chase) <edwin20100@gmail.com>
2016-11-26 06:43:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ var messages = [{
   posicionY: "Carlos Azaustre"
 }];
 
+var horarios =[{horaSalida: "6.45",horaLlegada: "7.05",periodo:"a.m.",tiempoRestante: "0 mins", estado:"Detenedio" },
+	      {horaSalida: "7.20",horaLlegada: "7.50",periodo:"a.m.",tiempoRestante: "0 mins", estado:"Detenedio" },
+	      {horaSalida: "8.50",horaLlegada: "9.10",periodo:"a.m.",tiempoRestante: "0 mins", estado:"Detenedio" },
+	      {horaSalida: "10.30",horaLlegada: "10.50",periodo:"a.m.",tiempoRestante: "0 mins", estado:"Detenedio" }];
+
 app.get('/hello', function(req, res) {  
   res.status(200).send("Hello World!");
 });
@@ -40,7 +45,8 @@ io.on('connection', function(socket){
 	console.log('Usuario Conectado...');
 	//console.log(socket);
     socketCount++;// Socket has connected, increase socket count
-	io.sockets.emit('usuario conectado', socketCount +'Hola');    // Let all sockets know how many are connected
+	//io.sockets.emit('usuario conectado', socketCount +'Hola');    // Let all sockets know how many are connected
+	io.sockets.emit('horarios',horarios); //emitos horarios a main.js
 	// ----->
 	 //socket.emit('messages', messages);
 
